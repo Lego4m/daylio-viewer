39b071ab1a4ddca5aadad9fcb78727ed3950b413
Leonardo Maron <leo22.maron@hotmail.com>
2020-08-22 20:40:21 -0300

    Updates

---
+++ precache-manifest.e3b63963188ee29363d17fe104ddd82c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d41e8b7317b65074f958033f7d3223",
+    "url": "/daylio-viewer/index.html"
+  },
+  {
+    "revision": "f31fa21b8034f1fc8338",
+    "url": "/daylio-viewer/static/js/2.f2576b4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/daylio-viewer/static/js/2.f2576b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7c6b60d30fbd497ddb",
+    "url": "/daylio-viewer/static/js/main.a41e842b.chunk.js"
+  },
+  {
+    "revision": "e08f6a31f32843355633",
+    "url": "/daylio-viewer/static/js/runtime-main.0da4146c.js"
+  },
+  {
+    "revision": "0fce98babaa343d657a168f1ce77a97b",
+    "url": "/daylio-viewer/static/media/awful.0fce98ba.svg"
+  },
+  {
+    "revision": "aec34ea031ba95e6f8f860d4912552d8",
+    "url": "/daylio-viewer/static/media/bad.aec34ea0.svg"
+  },
+  {
+    "revision": "3e71333ac0e60470f2cb8a60f0eb7a17",
+    "url": "/daylio-viewer/static/media/good.3e71333a.svg"
+  },
+  {
+    "revision": "e90b4c45ab87446ddca71b473fc5dd95",
+    "url": "/daylio-viewer/static/media/meh.e90b4c45.svg"
+  },
+  {
+    "revision": "b2271f4f1db2ab30f4774272b94656d9",
+    "url": "/daylio-viewer/static/media/rad.b2271f4f.svg"
+  }
+]);
