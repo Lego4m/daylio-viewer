a0ba8a3a82d5d377b5e617fad7560a9e169b5975
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-28 10:16:25 -0300

    Base - Rewrite on typescript

--- src/styles/colors.js
+++
@@ -1,5 +0,0 @@
-const moodColors = ['#fd8d02', '#41a669', '#8f54a5', '#5579a6', '#707b7c'];
-
-export function getMoodColor(groupId) {
-  return moodColors[groupId - 1] || moodColors[0];
-}
--- src/utils/getPredefinedName.js
+++
@@ -1,5 +0,0 @@
-const names = ['rad', 'good', 'meh', 'bad', 'awful'];
-
-export default function getPredefinedName(id) {
-  return names[id - 1] || names[0];
-}
